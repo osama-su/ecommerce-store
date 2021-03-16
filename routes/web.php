ef2c98af92393f6e4d71368421223b6f19cbf158
Osama Ahmed <osamaahmed.sus@gmail.com>
2021-03-16 14:23:49 +0200

    prepairing admin dashboard theme

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('front.home');
 });
 
+
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
