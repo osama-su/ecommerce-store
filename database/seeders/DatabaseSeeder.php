e802a7410310b90ddb55ae95ed73ee4419cb77a2
Osama Ahmed <osamaahmed.sus@gmail.com>
2021-04-04 10:57:30 +0200

    add seeder for admin record and added root folder for vendors images

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('admins')->insert([
+            'name' => Str::random(10),
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('123456'),
+        ]);
     }
 }
