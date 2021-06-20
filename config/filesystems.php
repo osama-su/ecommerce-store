2ae1de7a080b3769abf7ca07c4d2849b1b331158
Osama Ahmed <osamaahmed.sus@gmail.com>
2021-06-20 17:56:32 +0200

    restructure files

--- config/filesystems.php
+++ config/filesystems.php
@@ -43,13 +43,13 @@ return [
         ],
         'maincategories' => [
             'driver' => 'local',
-            'root' => base_path('/assets/images/maincategories/'),
+            'root' => public_path('assets/images/maincategories/'),
             'url' => env('APP_URL').'/public',
             'visibility' => 'public',
         ],
         'vendors' => [
             'driver' => 'local',
-            'root' => base_path('/assets/images/vendors/'),
+            'root' => public_path('assets/images/vendors/'),
             'url' => env('APP_URL').'/public',
             'visibility' => 'public',
         ],
